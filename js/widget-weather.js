b89daf04da15fe1ce2bda9c440790ec1f7309fd6
David Teruel <d_teruel@hotmail.com>
2019-12-23 13:05:55 +0100

    Guardo cambios leccion 2

--- js/widget-weather.js
+++ js/widget-weather.js
@@ -1,7 +1,7 @@
-const API_KEY = '627eb53a00b46c56672e5fef2aa41986'
-const service = new WeatherApiService(API_KEY)
+const API_KEY = '627eb53a00b46c56672e5fef2aa41986';
+const service = new WeatherApiService(API_KEY);
 
-const DEFAULT_QUERY='Barcelona'
+const DEFAULT_QUERY='Barcelona';
 
 new Vue({
   el: "#widget",
@@ -11,20 +11,21 @@ new Vue({
     temp_max: 0,
     temp_min: 0,
   },
-  created: function () { //ciclo de vida de las instancias de Vue
-    service.findWeather(DEFAULT_QUERY)
-      .then(this.setWeatherData)
+  created: function () { //Método que se llama síncronamente cuando se crea la instancia
+      service.findWeather(DEFAULT_QUERY)
+      .then(this.setWeatherData) //Promesa
   },
-  methods: {
+  methods: { // A estos métodos se les bindea automáticamente el this de la instancia (por eso desde ellos podemos acceder tranquilamente a this.query, this.setWeatherData, etc…)
     findWeather: function() {
       service.findWeather(this.query)
         .then(this.setWeatherData)
     },
     setWeatherData: function({ temp, temp_max, temp_min }) {
-      this.temp = temp
-      this.temp_max = temp_max
-      this.temp_min = temp_min
+      this.temp = temp;
+      this.temp_max = temp_max;
+      this.temp_min = temp_min;
     }
   }
 });
+
 Vue.config.devtools = true;

fd272a637598dd4656d76957c43a04f7af348623
David Teruel <d_teruel@hotmail.com>
2019-12-24 13:14:32 +0100

    Cap5: Componente commentsList que usa el componente CommentsItem para pintar los datos del array

--- js/widget-comments.js
+++ js/widget-comments.js
@@ -1,3 +1,29 @@
+const comments_data = [
+    {
+        id: 1,
+        username: "admin",
+        date: "Today, 2:30",
+        comment: "Comentario "
+    },
+    {
+        id: 2,
+        avatar: "http://demos.themes.guide/bodeo/assets/images/users/m105.jpg",
+        username: "admin",
+        date: "Today, 3:30",
+        comment: "Comentario "
+    },
+    {
+        id: 3,
+        username: "admin",
+        avatar: "http://demos.themes.guide/bodeo/assets/images/users/m104.jpg",
+        date: "Today, 4:30",
+        comment: "Comentario "
+    },
+];
+
 new Vue({
-  el: "#comments_block",
+    el: "#comments_block",
+    data: {
+        comments: comments_data
+    }
 });
